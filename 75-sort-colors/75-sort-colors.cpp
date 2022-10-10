1628855edf5303e0c8bedf033d99a07abf2ad39d
SHAKE ABDUL RUB <86040897+ShakeAbdulRub@users.noreply.github.com>
2022-10-10 16:30:10 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (69.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -15,7 +15,7 @@ public:
           {
               mid++;
             }
-          else if(nums[mid]==2)
+          else
           {
              swap(nums[mid],nums[high]);
               high--;
